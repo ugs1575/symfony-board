a7a4ffb74d5fb8365b33cd84709efe57626a4987
우경서 (Gyeongseo Woo) <ugs1575@gmail.com>
2025-05-01 18:17:58 +0900

    put, get, delete test code

--- tests/AppBundle/Controller/Post/PostControllerTest.php
+++ tests/AppBundle/Controller/Post/PostControllerTest.php
@@ -2,46 +2,136 @@
 
 namespace Tests\AppBundle\Controller\Post;
 
+use AppBundle\Entity\Post;
+use AppBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
 
 class PostControllerTest extends WebTestCase
 {
-    private $em;
+    private static $em;
 
-    protected function setUp()
+    private static $user;
+
+    public static function setUpBeforeClass()
     {
+        parent::setUpBeforeClass();
+
         self::bootKernel();
-        $this->em = self::$kernel->getContainer()->get('doctrine.orm.entity_manager');
+        self::$em = self::$kernel->getContainer()->get('doctrine.orm.entity_manager');
+
+        $user = new User();
+        self::$em->persist($user);
+        self::$em->flush();
+
+        self::$user = $user;
     }
 
     public function tearDown()
     {
-        $this->em->createQuery('DELETE FROM AppBundle:Post')->execute();
+        self::$em->createQuery('DELETE FROM AppBundle:Post')->execute();
     }
 
-    public function testPostPostsAction()
+    public function testPostAction()
     {
+        //when
         $client = static::createClient();
 
         $client->request('POST', '/posts', [
             'title' => 'test title',
             'content' => 'hello world',
-            'user' => 1
+            'user' => self::$user->getId()
         ]);
 
-        $this->assertEquals(201, $client->getResponse()->getStatusCode(), 'Response content: ' . $client->getResponse()->getContent());
-        $response = $client->getResponse();
-        print_r($response->getContent());
+        //then
+        $clientResponse = $client->getResponse();
+        $response = json_decode($clientResponse->getContent(), true);
 
-        $this->assertEquals(Response::HTTP_CREATED, $response->getStatusCode());
+        $this->assertEquals(Response::HTTP_CREATED, $clientResponse->getStatusCode());
 
-        $data = json_decode($response->getContent(), true);
+        $this->assertArrayHasKey('id', $response);
 
-        $this->assertArrayHasKey('id', $data);
-//        $this->assertSame('테스트 제목', $data['title']);
-//        $this->assertSame('테스트 제목', $data['title']);
-//        $this->assertSame('테스트 내용', $data['content']);
+        $post = self::$em->getRepository('AppBundle:Post')->find($response['id']);
+        $this->assertEquals('test title', $post->getTitle());
+        $this->assertEquals('hello world', $post->getContent());
+        $this->assertEquals(self::$user->getId(), $post->getUser()->getId());
+    }
+
+    public function testPutAction()
+    {
+        //given
+        $user = self::$em->getRepository(User::class)->find(self::$user->getId());
+        $post = new Post($user, "test title", "hello world");
+        self::$em->persist($post);
+        self::$em->flush();
+
+        //when
+        $client = static::createClient();
+
+        $client->request('PUT', '/posts/' . $post->getId(), [
+            'title' => 'test title changed',
+            'content' => 'hello world changed',
+            'user' => $user->getId()
+        ]);
+
+        //then
+        $clientResponse = $client->getResponse();
+        $response = json_decode($clientResponse->getContent(), true);
+
+        $this->assertEquals(Response::HTTP_OK, $clientResponse->getStatusCode());
+
+        $this->assertEquals($post->getId(), $response['id']);
+        $this->assertEquals('test title changed', $response['title']);
+        $this->assertEquals('hello world changed', $response['content']);
+        $this->assertEquals($user->getId(), $response['user']['id']);
+    }
+
+    public function testGetAction()
+    {
+        //given
+        $user = self::$em->getRepository(User::class)->find(self::$user->getId());
+        $post = new Post($user, "test title", "hello world");
+        self::$em->persist($post);
+        self::$em->flush();
+
+        //when
+        $client = static::createClient();
+
+        $client->request('GET', '/posts/' . $post->getId());
+
+        //then
+        $clientResponse = $client->getResponse();
+        $response = json_decode($clientResponse->getContent(), true);
+
+        $this->assertEquals(Response::HTTP_OK, $clientResponse->getStatusCode());
+
+        $this->assertEquals($post->getId(), $response['id']);
+        $this->assertEquals('test title', $response['title']);
+        $this->assertEquals('hello world', $response['content']);
+        $this->assertEquals($user->getId(), $response['user']['id']);
+    }
+
+    public function testDeleteAction()
+    {
+        //given
+        $user = self::$em->getRepository(User::class)->find(self::$user->getId());
+        $post = new Post($user, "test title", "hello world");
+        self::$em->persist($post);
+        self::$em->flush();
+
+        //when
+        $client = static::createClient();
+
+        $client->request('DELETE', '/posts/' . $post->getId());
+
+        //then
+        $clientResponse = $client->getResponse();
+        $response = json_decode($clientResponse->getContent(), true);
+
+        $this->assertEquals(Response::HTTP_NO_CONTENT, $clientResponse->getStatusCode());
+
+        $posts = self::$em->getRepository('AppBundle:Post')->findAll();
+        $this->assertEmpty($posts);
     }
 
 }
